0c7f4288b35cbb64d0a3069b542e2eed3bdd2f15
Brian Abent <brian.abent@gmail.com>
2016-05-27 17:22:12 -0400

    fixed a misnamed variable

--- src/highlander/highlander.js
+++ src/highlander/highlander.js
@@ -30,10 +30,10 @@
     function activateHighlander() {
         CerosSDK.findExperience().done(function(cerosExperience) {
             var pluginScriptTag = document.getElementById("ceros-highlander-plugin");
-            var groupsOfPictures =pluginScriptTag.getAttribute("highlanderTags").split(',');
+            var groupsOfPictures = pluginScriptTag.getAttribute("highlanderTags").split(',');
 
             jQuery.each(groupsOfPictures, function(tagIndex, groupTag) {
-            	var layerCollection = experience.findLayersByTag(groupTag);
+            	var layerCollection = cerosExperience.findLayersByTag(groupTag);
             	layerCollection.subscribe(CerosSDK.EVENTS.SHOWN, function(theVisibleLayer) {
             		jQuery.each(layerCollection.layers, function(layerIndex, someLayerInTheGroup) {
             			if (someLayerInTheGroup != theVisibleLayer) {
