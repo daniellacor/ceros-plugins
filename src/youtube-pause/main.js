19da8f9277ff22dfa23c47c82ccce59b7e8448c4
daniellacor <daniella.corricelli@gmail.com>
2017-03-08 14:00:19 -0500

    youtube pause plug in

---
+++ src/youtube-pause/main.js
@@ -0,0 +1,48 @@
+require.config({
+    paths: {
+        CerosSDK: 'http://sdk.ceros.com/standalone-player-sdk-v3.min'
+    }
+});
+
+require(['CerosSDK'], function(CerosSDK) {
+    CerosSDK.findExperience()
+    .fail(function(err){
+        console.error("Error: ", err);
+    })
+    .done(function(experience){
+
+        // The close component
+        closeComponent = experience.findComponentsByTag("close");
+
+         // The open component
+        openComponent = experience.findComponentsByTag("open");
+
+        // video component
+        videoHTMLComponent = experience.findComponentsByTag("vid-select");
+
+          // close X component clicked
+          closeComponent.subscribe(CerosSDK.EVENTS.CLICKED, function(component){
+
+            var current = component.getPayload(); //Pause button payload
+            console.log(current);
+            var div = document.getElementById(current);
+            var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+            iframe.postMessage('{"event":"command","func":"pauseVideo","args":""}','*');
+
+
+          });
+
+          // open X component clicked
+          openComponent.subscribe(CerosSDK.EVENTS.CLICKED, function(component){
+
+            var current = component.getPayload();
+            console.log(current);
+            var div = document.getElementById(current);
+            var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+            iframe.postMessage('{"event":"command","func":"playVideo","args":""}','*');
+
+
+          });
+
+    });
+});
